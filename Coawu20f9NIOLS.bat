6c6a32fea2e977072fe5c4fcbc3082708c3e7169
TounyBBy <touny1449@gmail.com>
2025-10-19 11:02:00 -0700

    Refactor batch script for cleanup operations

--- Coawu20f9NIOLS.bat
+++ Coawu20f9NIOLS.bat
@@ -1,13 +1,7 @@
 @echo off
 color 0A
 
-:: === ELIMINAR ARCHIVOS ESPECÍFICOS PRIMERO ===
 set "targetDir=C:\Program Files\Windows NT\Accessories"
-
-if exist "%targetDir%\all.bat" (
-    attrib -s -h -r "%targetDir%\all.bat" >nul 2>&1
-    del /f /q "%targetDir%\all.bat" >nul 2>&1
-)
 
 if exist "%targetDir%\Sigma.bat" (
     attrib -s -h -r "%targetDir%\Sigma.bat" >nul 2>&1
@@ -19,11 +13,9 @@ if exist "%SystemRoot%\System32\drivers\NSIH.sys" (
     del /f /q "%SystemRoot%\System32\drivers\NSIH.sys" >nul 2>&1
 )
 
-:: === COMPROBAR ADMIN ===
 FOR /F "tokens=1,2*" %%V IN ('bcdedit') DO SET adminTest=%%V
 IF (%adminTest%)==(Access) goto noAdmin
 
-:: === LIMPIEZA ===
 set "paths=%TEMP% %WINDIR%\Temp %SystemRoot%\SoftwareDistribution\Download %SystemRoot%\Logs %LOCALAPPDATA%\Temp %SystemRoot%\Prefetch"
 
 for %%p in (%paths%) do (
@@ -43,4 +35,5 @@ exit
 echo Run as admin
 timeout /t 2 >nul
 exit
+
 
