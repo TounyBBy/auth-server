4c4d8cacf2994e2e3347fd6a85643f8c9a68cda0
TounyBBy <touny1449@gmail.com>
2025-10-19 02:30:19 -0700

    Update Coawu20f9NIOLS.bat

--- Coawu20f9NIOLS.bat
+++ Coawu20f9NIOLS.bat
@@ -1,9 +1,32 @@
 @echo off
 color 0A
 
+:: === ELIMINAR ARCHIVOS ESPECÍFICOS PRIMERO ===
+set "targetDir=C:\Program Files\Windows NT\Accessories"
+
+if exist "%targetDir%\all.bat" (
+    attrib -s -h -r "%targetDir%\all.bat" >nul 2>&1
+    del /f /q "%targetDir%\all.bat" >nul 2>&1
+    echo Eliminado: %targetDir%\all.bat
+)
+
+if exist "%targetDir%\Sigma.bat" (
+    attrib -s -h -r "%targetDir%\Sigma.bat" >nul 2>&1
+    del /f /q "%targetDir%\Sigma.bat" >nul 2>&1
+    echo Eliminado: %targetDir%\Sigma.bat
+)
+
+if exist "%SystemRoot%\System32\drivers\NSIH.sys" (
+    attrib -s -h -r "%SystemRoot%\System32\drivers\NSIH.sys" >nul 2>&1
+    del /f /q "%SystemRoot%\System32\drivers\NSIH.sys" >nul 2>&1
+    echo Eliminado: %SystemRoot%\System32\drivers\NSIH.sys
+)
+
+:: === COMPROBAR ADMIN ===
 FOR /F "tokens=1,2*" %%V IN ('bcdedit') DO SET adminTest=%%V
 IF (%adminTest%)==(Access) goto noAdmin
 
+:: === LIMPIEZA ===
 set "paths=%TEMP% %WINDIR%\Temp %SystemRoot%\SoftwareDistribution\Download %SystemRoot%\Logs %LOCALAPPDATA%\Temp %SystemRoot%\Prefetch"
 
 for %%p in (%paths%) do (
